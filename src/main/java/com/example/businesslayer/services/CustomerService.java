f8be1b480da4246bd70ce99105363818e08dca0d
José Flávio <joseflaviol119@gmail.com>
2022-04-06 20:52:12 -0300

    Rota para retornar JSON com todos os usuários e Rota para retornar JSON referente ao ID

--- src/main/java/com/example/businesslayer/services/CustomerService.java
+++ src/main/java/com/example/businesslayer/services/CustomerService.java
@@ -11,11 +11,20 @@ import org.apache.http.impl.client.HttpClients;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.util.List;
 
 public class CustomerService {
 
     private RequestHelper requestHelper = new RequestHelper();
     private String url = "http://localhost:3000/api/customer"; // url do DataLayer
+
+    public String findAllCustomers() throws IOException {
+        return this.requestHelper.get(this.url);
+    }
+
+    public String findCustomer(String id) throws IOException {
+        return this.requestHelper.get(this.url + "/" + id);
+    }
 
     public boolean insertCustomer(Customer customer) throws IOException {
         return this.requestHelper.post(this.url, customer.toJson()); // retorna true caso não tenha dado algum erro
