f337dca2d5aff8ca5e9445d88cfab235b46adb16
José Flávio <joseflaviol119@gmail.com>
2022-04-06 22:02:15 -0300

    Insere usuário. Parte do login precisa ser mudada.

---
+++ src/main/java/com/example/businesslayer/resources/UserResource.java
@@ -0,0 +1,37 @@
+package com.example.businesslayer.resources;
+
+import com.example.businesslayer.models.User;
+import com.example.businesslayer.services.UserService;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import java.io.IOException;
+
+@Path("user")
+public class UserResource {
+
+    private UserService userService = new UserService();
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createUser(User user) throws IOException {
+        if (this.userService.insertUser(user)) {
+            return Response.status(200).build();
+        }
+        return Response.status(400).build();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/login")
+    public Response login(User user) throws IOException {
+        if (this.userService.login(user)) {
+            return Response.status(200).build();
+        }
+        return Response.status(400).build();
+    }
+
+}
