ff9a95b38efe124888f1bd659a3f551a6054f1a5
José Flávio <joseflaviol119@gmail.com>
2022-04-08 21:19:08 -0300

    Criação da classe "ErrorHelper"

--- src/main/java/com/example/businesslayer/services/CustomerService.java
+++ src/main/java/com/example/businesslayer/services/CustomerService.java
@@ -2,32 +2,25 @@ package com.example.businesslayer.services;
 
 import com.example.businesslayer.helpers.RequestHelper;
 import com.example.businesslayer.models.Customer;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.util.List;
 
 public class CustomerService {
 
     private RequestHelper requestHelper = new RequestHelper();
-    private String url = "http://0226-170-254-198-77.ngrok.io/SparklingWater/api/customer"; // url do DataLayer
+    private String url = "https://sparklingwaterapi.herokuapp.com/api/customer"; // url do DataLayer
 
-    public String findAllCustomers(int range) throws IOException {
-        return this.requestHelper.get(this.url + "/select/paginated/" + range);
+    public String findAllCustomers(/*int range*/) throws IOException {
+        return this.requestHelper.get(this.url);
+                //return this.requestHelper.get(this.url + "/select/paginated/" + range);
     }
 
     public String findCustomer(String id) throws IOException {
-        return this.requestHelper.get(this.url + "/select/" + id);
+        return this.requestHelper.get(this.url + "/" + id);// + "/select/" + id);
     }
 
     public boolean insertCustomer(Customer customer) throws IOException {
-        return this.requestHelper.post(this.url + "/insert", customer.toJson()); // retorna true caso não tenha dado algum erro
+        return this.requestHelper.post(this.url, customer.toJson());// "/insert", customer.toJson()); // retorna true caso não tenha dado algum erro
     }
 
 }
